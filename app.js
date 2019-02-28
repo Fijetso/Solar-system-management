fbcac86bd67da862f3e8ac2ee61b31b1cb7f0b30
Fijetso <henrynguyen02081997@gmail.com>
2019-02-28 09:15:35 +0700

    setup socketio

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 var express = require("express");
 var path = require("path");
-
 var indexRouter = require("./routes/index/home");
 var app = express();
 
-// view engine setup
+///Socket.io setup
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+///view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // app.use(logger("dev"));
+// app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+///Listen to socket.io
+io.on("connection", function(socket) {
+  console.log("Da ket noi " + socket.id);
+  socket.on("disconnect", function() {
+    console.log(socket.id + " da ngat ket noi");
+  });
+});
 
 app.use("/", indexRouter);
 
-// catch 404 and forward to error handler
+///catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -32,6 +42,6 @@ app.use(function(err, req, res, next) {
   res.render("error");
 });
 
-app.listen(3000, process.env.IP, function() {
+server.listen(3000, process.env.IP, function() {
   console.log("Visit server at: " + process.env.PORT);
 });
